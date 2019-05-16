92f76a3f05679077b4f6cb18bab48e073bed9ec1
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-05-16 12:16:57 +0100

    repository initialized

---
+++ themes/mytheme/footer.php
@@ -0,0 +1,25 @@
+<p>Hello world! This is HTML5 Boilerplate.</p>
+</div>
+<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr-3.7.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script>
+  window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js / vendor / jquery - 3.3 .1.min.js "><\/script>')
+</script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+  window.ga = function() {
+    ga.q.push(arguments)
+  };
+  ga.q = [];
+  ga.l = +new Date;
+  ga('create', 'UA-XXXXX-Y', 'auto');
+  ga('set', 'transport', 'beacon');
+  ga('send', 'pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+
+</html>
